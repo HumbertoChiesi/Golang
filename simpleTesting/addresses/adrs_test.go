4141c323420e2b7d6b9b276160fd9a8ae65bebc9
HumbertoChiesi <66853228+HumbertoChiesi@users.noreply.github.com>
2021-08-30 20:38:59 -0300

    test cases

--- simpleTesting/addresses/adrs_test.go
+++ simpleTesting/addresses/adrs_test.go
@@ -3,12 +3,29 @@ package addresses
 
 import "testing"
 
-func TestAddressType(t *testing.T) {
-	addresForTest := "Avenida Paulista"
-	expectedAddressType := "Avenida"
-	receivedAddressType := AddressType(addresForTest)
+type testCase struct {
+	addressInput   string
+	expectedReturn string
+}
 
-	if receivedAddressType != expectedAddressType {
-		t.Error("O tipo recebido é diferente do esperado!")
+func TestAddressType(t *testing.T) {
+	testCases := []testCase{
+		{"Rua Apinajes", "Rua"},
+		{"Rodovia BR21", "Rodovia"},
+		{"Avenida Paulista", "Avenida"},
+		{"Praça da Sé", "Praça"},
+		{"Estrada da noite", "Estrada"},
+		{"RUA SANTA VIRGINIA", "Rua"},
+		{"Planalto ABC", "Invalid address type"},
+	}
+
+	for _, cases := range testCases {
+		receivedAddressType := AddressType(cases.addressInput)
+		if receivedAddressType != cases.expectedReturn {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s",
+				receivedAddressType,
+				cases.expectedReturn,
+			)
+		}
 	}
 }
